ded754b032e02aa0751a7058b6903f0d5644bc20
Muzaffer <Muzaffer@MUZAFFAR-LAP>
2025-05-23 11:19:05 +0300

    Add project files.

---
+++ LeaveManagmentSystem.web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagmentSystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel()
+            {
+                Name = "Muzaffar Mansour",
+                DateofBirth = new DateTime(1987, 07, 11)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagmentSystem.web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagmentSystem.web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateofBirth { get; set; }
+    }
+}
